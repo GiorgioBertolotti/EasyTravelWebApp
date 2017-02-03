5c5b7050bcd302e77b1b7f259b3d0c90f633c9d0
Giorgio <giorgiobertol@gmail.com>
2017-02-03 16:27:05 +0100

    Init progetto

---
+++ EasyTravel/PAGES/Component.js
@@ -0,0 +1,25 @@
+﻿sap.ui.define(['jquery.sap.global', 'sap/ui/core/UIComponent'],
+	function (jQuery, UIComponent) {
+	    "use strict";
+
+	    return UIComponent.extend("sap.ui.easytravel.login.Component", {
+	        metadata: {
+	            name: "Login",
+	            version: "1.0",
+	            includes: [],
+	            dependencies: {
+	                libs: ["sap.m", "sap.ui.layout"],
+	                components: []
+	            },
+	            rootView: "sap.ui.easytravel.login.Login"
+	            //config: {
+	            //    resourceName: "i18n",
+	            //    resourceBundle: "XXXX",
+	            //    serviceConfig: {
+	            //        name: "main",
+	            //        serviceUrl: "XXXXX",
+	            //    }
+	            //}
+	        }
+	    });
+	});
---
+++ EasyTravel/PAGES/Login.controller.js
@@ -0,0 +1,13 @@
+﻿sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+    var oView;
+    return Controller.extend("sap.ui.easytravel.login.Login", {
+        onInit: function () {
+            oView = this.getView();
+            oView.byId("imglogo").addStyleClass("myLogo");
+        }
+    });
+});
