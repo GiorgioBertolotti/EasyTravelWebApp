3e2f54dd736626572932410d0065ea11f0d82c37
Giorgio <giorgiobertol@gmail.com>
2017-02-03 16:31:34 +0100

    Migliorato layout pagina login

---
+++ EasyTravel/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using EasyTravel.Properties;
+namespace EasyTravel.Controllers
+{
+    public class LoginController : ApiController
+    {
+        [HttpGet]
+        public string setIP(string ip)
+        {
+            string result = "ok";
+            try
+            {
+                string json = File.ReadAllText("../Content/Setting.json");
+                dynamic jsonObj = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+                jsonObj["ip"] = ip;
+                string output = Newtonsoft.Json.JsonConvert.SerializeObject(jsonObj, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText("../Content/Setting.json", output);
+            }
+            catch(Exception e)
+            {
+                result = e.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ EasyTravel/PAGES/Register.controller.js
@@ -0,0 +1,15 @@
+﻿sap.ui.define([
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+    var oView;
+    return Controller.extend("sap.ui.easytravel.register.Register", {
+        onInit: function () {
+        },
+        onBtnBack: function (oEvent) {
+            var app = sap.ui.getCore().byId("EasyTravel");
+            app.to("loginPage", "flip");
+        }
+    });
+});
